44b8946f8eac732dd162e90551d22d95a05e4abd
Shreya Koley <68245668+shreyakoley020102@users.noreply.github.com>
2021-02-04 20:14:24 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app=express();
+
+app.get("/", function(request, response){
+  response.send("Hello, world!");
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000");
+});
